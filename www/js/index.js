220a9c438f7c5e68a80c3d5736fa725382d49efb
Stéphane JAIS <stephane@cantinasoftware.com>
2017-03-30 12:28:01 +0200

    custom keyboard demo

--- www/js/index.js
+++ www/js/index.js
@@ -27,11 +27,27 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        console.log('hello');
+        var openKeyboardListener = function(result) {
+          console.log('open');
+          console.log(result);
+        };
+        var closeKeyboardListener = function(result) {
+          console.log('close');
+          console.log(result);
+        };
+        CustomKeyboard.setOpenCloseListener(openKeyboardListener, closeKeyboardListener);
+        setTimeout(function() {
+                   console.log("unset");
+                   CustomKeyboard.unsetOpenCloseListener();
+                   }, 4000);
+        console.log('done');
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        return;
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -44,3 +60,22 @@ var app = {
 };
 
 app.initialize();
+
+var toggle = document.getElementById('toggle');
+var output = document.getElementById('output');
+
+toggle.addEventListener('click', function() {
+  var elt = this;
+  if (elt.className == 'show')  {
+    CustomKeyboard.open(123456, 9, function(val) {console.log(val);});
+
+    elt.className = 'hide';
+    elt.innerHTML = 'Hide keyboard';
+  } else {
+    CustomKeyboard.close()
+    elt.className = 'show';
+    elt.innerHTML = 'Show keyboard.';
+  }
+
+});
+
